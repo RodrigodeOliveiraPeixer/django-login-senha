a1f3690dba7d948279d360104fd29aa6b530220f
RodrigodeOliveiraPeixer <rodrigo.oliveira.peixer@gmail.com>
2022-11-21 15:49:52 -0300

    atualização-logout-aleração-senha

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 
 
@@ -47,3 +47,18 @@ def dologin(request):
 #Página inicial do dashboard
 def dashboard(request):
     return render(request,'dashboard/home.html')
+
+#logout do sistema
+def logouts(request):
+    logout(request)
+    return redirect('/painel/')
+
+#Alterar a senha
+def changePassword(request):
+    user = User.objects.get(email=request.user.email)
+    user.set_password(request.POST['password'])
+    user.save()
+    logout(request)
+    return redirect('/painel/')
+    
+
